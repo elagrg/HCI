c3240848ece44aefd2156d267f69df430b3d4889
elagrg <egrgur00@fesb.hr>
2021-10-19 02:21:37 +0200

    Add messy code

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to 
-          <a href="https://nextjs.org">Korisnička sučelja!</a>
+          Welcome to  
+           <a href="https://nextjs.org">Korisnička sučelja!
+                      </a>
         </h1>
 
         <p className={styles.description}>
