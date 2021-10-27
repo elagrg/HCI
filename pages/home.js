e30b4b699a3e04874b928d8e99bb95b0e6a22253
elagrg <egrgur00@fesb.hr>
2021-10-27 06:33:11 +0200

    Add header

---
+++ pages/home.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+
+// function Home() {
+//     return <h1>Hello home</h1>;
+// }
+import Header from "../modules/header/header";
+
+const Home = () => <Header />;
+
+export default Home;
